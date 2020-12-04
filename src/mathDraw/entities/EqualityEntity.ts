005a501ea6ab326ad25e267fa89abe3d71650bec
Branislav Trstenský <bt7s7k7@hotmail.sk>
2020-12-04 13:09:23 +0100

    Fixed implicit multiplication with more than two identifiers

--- src/mathDraw/entities/EqualityEntity.ts
+++ src/mathDraw/entities/EqualityEntity.ts
@@ -12,22 +12,23 @@ export namespace EqualityEntity {
 
         const parseExpression = (expression: Expression) => {
             if (expression.type == "BinaryExpression") {
-                if (expression.operator == "*" && expression.left.type == "Literal" && expression.right.range![0] == expression.left.range![1] + expression.operator.length + 2 + 1) {
-                    parseExpression(expression.left)
-                    ret.push("(")
-                    parseExpression(expression.right)
-                    ret.push(")")
-                } else if (expression.operator == "*" && (expression.left.type == "Literal" || expression.left.type == "Identifier") && (expression.right.type == "Identifier" || expression.right.type == "BinaryExpression" && expression.right.operator == "**")) {
+                if (expression.operator == "*" && (expression.left.type == "Literal" || expression.left.type == "Identifier") && (expression.right.type == "Identifier" || expression.right.type == "BinaryExpression" && (expression.right.operator == "**" || expression.right.operator == "*"))) {
                     parseExpression(expression.left)
                     parseExpression(expression.right)
-                } else {
-                    parseExpression(expression.left)
+                } else
+                    if (expression.operator == "*" && expression.left.type == "Literal" && expression.right.range![0] == expression.left.range![1] + expression.operator.length + 2 + 1) {
+                        parseExpression(expression.left)
+                        ret.push("(")
+                        parseExpression(expression.right)
+                        ret.push(")")
+                    } else {
+                        parseExpression(expression.left)
 
-                    if (expression.operator == "**") ret.push("^")
-                    else ret.push(expression.operator)
+                        if (expression.operator == "**") ret.push("^")
+                        else ret.push(expression.operator)
 
-                    parseExpression(expression.right)
-                }
+                        parseExpression(expression.right)
+                    }
             } else if (expression.type == "Identifier") {
                 ret.push(expression.name)
             } else if (expression.type == "Literal") {
