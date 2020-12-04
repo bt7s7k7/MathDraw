6b6443b8817f7b18f020abc0fd4a02647495ac02
Branislav Trstenský <bt7s7k7@hotmail.sk>
2020-12-04 13:40:44 +0100

    Added more functions

--- src/mathDraw/components/Renderer.tsx
+++ src/mathDraw/components/Renderer.tsx
@@ -36,7 +36,7 @@ export const Renderer = defineComponent({
                     
                         return ret.join(" ")
                     }`,
-                    ...["sin", "cos", "tan", "sqrt"].map(v => `var ${v} = Math.${v}`),
+                    ...["sin", "cos", "tan", "sqrt", "floor", "ceil", "round"].map(v => `var ${v} = Math.${v}`),
                     ...["sin", "cos", "tan"].map(v => `var arc${v} = Math.a${v}`),
                     "var TO_DEG = Math.PI / 180",
                     "var pi = Math.PI",
